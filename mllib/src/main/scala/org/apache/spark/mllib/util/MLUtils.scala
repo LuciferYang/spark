dcf1c4761c55edc134bc7fbd726ec3560b691ff3
yangjie01 <yangjie01@baidu.com>
2023-10-28 18:30:36 +0800

    first part

--- mllib/src/main/scala/org/apache/spark/mllib/util/MLUtils.scala
+++ mllib/src/main/scala/org/apache/spark/mllib/util/MLUtils.scala
@@ -117,7 +117,7 @@ object MLUtils extends Logging {
       ).resolveRelation(checkFilesExist = false))
       .select("value")
 
-    import lines.sqlContext.implicits._
+    import lines.sparkSession.implicits._
 
     lines.select(trim($"value").as("line"))
       .filter(not((length($"line") === 0).or($"line".startsWith("#"))))
